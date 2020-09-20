6773ab70d5bf3cc86e210f502ea333894f7732c8
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-09-20 18:46:09 +0530

    Test cases added now

---
+++ src/test/java/org/scube/util/ScubeUtilTest.java
@@ -0,0 +1,15 @@
+package org.scube.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScubeUtilTest {
+
+    @Test
+    void writeCode() throws IOException, ClassNotFoundException {
+        ScubeUtil.generateCode("src/test/resources/mysql.dao","target/generated-sources/mydb/resources/mysql.dao");
+    }
+}
