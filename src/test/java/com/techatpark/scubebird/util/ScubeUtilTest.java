f0aea636803115ac676d675eba479266b0641c61
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-09-22 17:17:05 +0530

    Rearrange Mapping

--- src/test/java/com/techatpark/scubebird/util/ScubeUtilTest.java
+++ src/test/java/com/techatpark/scubebird/util/ScubeUtilTest.java
@@ -8,6 +8,6 @@ class ScubeUtilTest {
 
     @Test
     void writeCode() throws IOException, ClassNotFoundException {
-        ScubeUtil.generateCode("src/test/resources/mysql.dao","target/generated-sources/mydb");
+        ScubeUtil.generateCode("src/test/resources/sakila.dao","target/generated-sources/sakila");
     }
 }
