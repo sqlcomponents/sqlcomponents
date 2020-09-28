bd2878e89bfc16c1d5a236e57e538656af247c6a
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-09-28 09:49:28 +0530

    Psogress DB added

---
+++ postgres-crawler/src/test/java/com/techatpark/scubebird/crawler/postgres/PostgresCrawlerTest.java
@@ -0,0 +1,17 @@
+package com.techatpark.scubebird.crawler.postgres;
+
+import com.techatpark.scubebird.core.exception.ScubeException;
+import com.techatpark.scubebird.core.model.CrawlerConfig;
+import com.techatpark.scubebird.core.model.DaoProject;
+import com.techatpark.scubebird.core.model.Schema;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class PostgresCrawlerTest {
+
+    @Test
+    void testGetSchema() throws ScubeException {
+
+    }
+}
