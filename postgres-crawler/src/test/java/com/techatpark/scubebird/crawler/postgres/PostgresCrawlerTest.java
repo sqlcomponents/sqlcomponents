07b50d442aae556031ffee02d252187ee2aadb96
haripriya <rm.haripriya@gmail.com>
2020-10-06 17:38:38 +0530

    Clean up

--- postgres-crawler/src/test/java/com/techatpark/scubebird/crawler/postgres/PostgresCrawlerTest.java
+++ postgres-crawler/src/test/java/com/techatpark/scubebird/crawler/postgres/PostgresCrawlerTest.java
@@ -1,5 +1,6 @@
 package com.techatpark.scubebird.crawler.postgres;
 
+import com.techatpark.scubebird.core.crawler.Crawler;
 import com.techatpark.scubebird.core.exception.ScubeException;
 import com.techatpark.scubebird.core.model.CrawlerConfig;
 import com.techatpark.scubebird.core.model.DaoProject;
@@ -12,6 +13,23 @@ class PostgresCrawlerTest {
 
     @Test
     void testGetSchema() throws ScubeException {
+        DaoProject daoProject = new DaoProject();
 
+        daoProject.setName("Sakila");
+        daoProject.setUrl("jdbc:postgresql://localhost:5432/sakila");
+        daoProject.setUserName("postgres");
+        daoProject.setPassword("postgres");
+        daoProject.setSchemaName("sakila");
+        daoProject.setTablePatterns(List.of("%"));
+
+        CrawlerConfig crawlerConfig = new CrawlerConfig();
+        crawlerConfig.setClassName("com.techatpark.scubebird.crawler.postgres.PostgresCrawler");
+        crawlerConfig.setDriverName("org.postgresql.Driver");
+        crawlerConfig.setName("Postgres");
+        daoProject.setCrawlerConfig(crawlerConfig);
+
+        Crawler crawler = new PostgresCrawler();
+        Schema schema = crawler.getSchema(daoProject);
+        System.out.printf("schema");
     }
 }
