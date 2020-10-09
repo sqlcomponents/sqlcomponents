fff564b603c953922a7abc43ca332e47d4770640
haripriya <rm.haripriya@gmail.com>
2020-10-09 08:36:24 +0530

    API Structure for the Demo

--- java-compiler/src/test/java/com/techatpark/scubebird/core/implementation/util/ScubeUtilTest.java
+++ java-compiler/src/test/java/com/techatpark/scubebird/core/implementation/util/ScubeUtilTest.java
@@ -5,48 +5,85 @@ import com.techatpark.scubebird.core.exception.ScubeException;
 import com.techatpark.scubebird.core.implementation.mapper.Mapper;
 import com.techatpark.scubebird.core.implementation.mapper.java.JavaOrmMapper;
 import com.techatpark.scubebird.core.model.DaoProject;
-import com.techatpark.scubebird.core.model.Schema;
+import com.techatpark.scubebird.crawler.oracle.OracleCrawler;
 import com.techatpark.scubebird.crawler.postgres.PostgresCrawler;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.List;
 
 class ScubeUtilTest {
 
     @Test
-    void writeCode() throws IOException, ClassNotFoundException, ScubeException, SQLException {
-        DaoProject daoProject = new DaoProject();
+    void writeCode() throws ScubeException, SQLException {
+        new Application()
+                .connectToPostgress()
+                .understand()
+                .mapToJava()
+                .writeCode();
+    }
 
-        daoProject.setName("Sakila");
-        daoProject.setUrl("jdbc:postgresql://localhost:5432/sakila");
-        daoProject.setUserName("postgres");
-        daoProject.setPassword("postgres");
-        daoProject.setSchemaName("sakila");
-        daoProject.setTablePatterns(Arrays.asList("actor"));
+    class Application {
 
-        daoProject.setOnline(true);
+        private final DaoProject daoProject ;
 
-        daoProject.setBeanIdentifier("model");
-        daoProject.setDaoIdentifier("dao");
-        daoProject.setDaoSuffix("");
+        Application() {
+            this.daoProject = new DaoProject();;
+        }
 
-        daoProject.setMethodSpecification(Arrays.asList("DeleteByEntity", "DeleteByPK", "Without", "GetAll", "GetByEntity", "GetByPK", "GetByPKExceptHighest", "GetByPKUniqueKeys",
-                "InsertByEntiy", "IsExisting", "MViewRefresh", "SaveByPK", "UpdateByEntiy", "UpdateByPK"));
+        Application connectToPostgress() throws SQLException, ScubeException {
+            daoProject.setName("Movie");
+            daoProject.setUrl("jdbc:postgresql://localhost:5432/sakila");
+            daoProject.setUserName("postgres");
+            daoProject.setPassword("postgres");
+            daoProject.setSchemaName("sakila");
+            daoProject.setTablePatterns(Arrays.asList("%"));
 
-        daoProject.setRootPackage("org.example");
+            daoProject.setOnline(true);
+            daoProject.setBeanIdentifier("model");
+            daoProject.setDaoIdentifier("dao");
+            daoProject.setDaoSuffix("");
+            daoProject.setRootPackage("org.example");
+            daoProject.setCleanSource(true);
 
-        daoProject.setSrcFolder("/home/haripriya/Official/java-oracle-jdbc/target/generated-sources/sakila-postgres");
-        daoProject.setCleanSource(true);
 
-        Crawler crawler = new PostgresCrawler(daoProject);
-        Mapper mapper = new JavaOrmMapper();
-        daoProject.setOrm(mapper.getOrm(daoProject,crawler));
+            Crawler crawler = new PostgresCrawler(daoProject);
+            Mapper mapper = new JavaOrmMapper();
+            daoProject.setOrm(mapper.getOrm(daoProject,crawler));
+            return this;
+        }
 
-        ScubeUtil.writeCode(daoProject);
-        Assertions.assertNotNull(daoProject);
+        Application connectToOracle() throws SQLException, ScubeException {
+            daoProject.setName("Movie");
+            daoProject.setUrl("jdbc:oracle:thin:@localhost:51521:xe");
+            daoProject.setUserName("c##movie");
+            daoProject.setPassword("movie");
+            daoProject.setSchemaName("c##movie");
+            daoProject.setTablePatterns(Arrays.asList("%"));
+            return this;
+        }
+
+        Application understand() {
+
+            daoProject.setMethodSpecification(Arrays.asList("DeleteByEntity", "DeleteByPK", "Without", "GetAll",
+                    "GetByEntity", "GetByPK", "GetByPKExceptHighest", "GetByPKUniqueKeys",
+                    "InsertByEntiy", "IsExisting", "MViewRefresh", "SaveByPK", "UpdateByEntiy", "UpdateByPK"));
+            return this;
+        }
+
+        Application mapToJava() {
+
+            return this;
+        }
+
+        void writeCode() throws SQLException, ScubeException {
+            daoProject.setSrcFolder("/home/haripriya/Official/java-oracle-jdbc/target/generated-sources/sakila-postgres");
+
+            ScubeUtil.writeCode(daoProject);
+
+            System.out.println("Granted !");
+        }
+
+
     }
 }
