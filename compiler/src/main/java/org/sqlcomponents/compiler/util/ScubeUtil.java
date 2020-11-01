577eaf7776b05098e9aba0039706419d70e8f23a
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-11-01 09:05:20 +0530

    Removed File Util

--- compiler/src/main/java/org/sqlcomponents/compiler/util/ScubeUtil.java
+++ compiler/src/main/java/org/sqlcomponents/compiler/util/ScubeUtil.java
@@ -5,15 +5,23 @@ import org.sqlcomponents.compiler.OrmImplementation;
 import org.sqlcomponents.compiler.specification.Specification;
 import org.sqlcomponents.core.model.Application;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.SQLException;
+import java.util.Comparator;
 
 public class ScubeUtil {
 
 
-	public static void writeCode(Application application) throws ScubeException, SQLException {
+	public static void writeCode(Application application) throws ScubeException, SQLException, IOException {
 
 		if (application.isCleanSource()) {
-			FileUtil.deleteDir(application.getSrcFolder());
+			Files.walk(new File(application.getSrcFolder()).toPath())
+					.sorted(Comparator.reverseOrder())
+					.map(Path::toFile)
+					.forEach(File::delete);
 		}
 		Specification.getSpecification().writeSpecification(application);
 		OrmImplementation.getImplementation().writeImplementation(application);
