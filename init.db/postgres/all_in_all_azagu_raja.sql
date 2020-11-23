81d4955df0cfd3fc062754bd1e0f8f05b8309cab
Haripriya <rm.haripriya@gmail.com>
2020-11-23 16:22:08 +0530

    sql changes

--- init.db/postgres/all_in_all_azagu_raja.sql
+++ init.db/postgres/all_in_all_azagu_raja.sql
@@ -1,6 +1,30 @@
+
+DROP TABLE IF EXISTS all_in_all_azagu_raja;
 DROP TABLE IF EXISTS all_in_all_azagu_raja_reference;
+
+
 CREATE TABLE all_in_all_azagu_raja_reference (
     code VARCHAR(80) PRIMARY KEY,
     name VARCHAR(80),
     UNIQUE(name)
 );
+
+
+CREATE TABLE public.all_in_all_azagu_raja(
+   id SERIAL PRIMARY KEY,
+   reference_code VARCHAR(80),
+   CONSTRAINT fk_code
+      FOREIGN KEY(reference_code) 
+	  REFERENCES all_in_all_azagu_raja_reference(code)
+);
+
+
+INSERT INTO public.all_in_all_azagu_raja_reference
+(code, "name")
+VALUES('A110', 'Hari'),('A210', 'Priya'),('A310', 'Hansini');
+
+INSERT INTO public.all_in_all_azagu_raja
+(reference_code)
+VALUES('A110');
+
+
