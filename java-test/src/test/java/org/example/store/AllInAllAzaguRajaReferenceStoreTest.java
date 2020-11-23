5e8ca48c734ea8e3ac6bc30e947401cc0d4b57db
Haripriya <rm.haripriya@gmail.com>
2020-11-23 13:29:55 +0530

    reference store test

--- java-test/src/test/java/org/example/store/AllInAllAzaguRajaReferenceStoreTest.java
+++ java-test/src/test/java/org/example/store/AllInAllAzaguRajaReferenceStoreTest.java
@@ -2,7 +2,6 @@ package org.example.store;
 
 import static org.example.util.DataSourceProvider.dataSource;
 
-import org.example.model.AllInAllAzaguRaja;
 import org.example.model.AllInAllAzaguRajaReference;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,30 +14,22 @@ import java.sql.SQLException;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AllInAllAzaguRajaReferenceStoreTest {
 
-    private final AllInAllAzaguRajaStore allInAllAzaguRajaStore;
-    
-    
-    private AllInAllAzaguRaja allInAllAzaguRaja;
+    private final AllInAllAzaguRajaReferenceStore allAzaguRajaReferenceStore;
+
     private AllInAllAzaguRajaReference allInAllAzaguRajaReference;
     
     
     AllInAllAzaguRajaReferenceStoreTest() {
-        this.allInAllAzaguRajaStore = new AllInAllAzaguRajaStore(dataSource());
+        this.allAzaguRajaReferenceStore = new AllInAllAzaguRajaReferenceStore(dataSource());
     }
 
     @BeforeAll
     void init() throws SQLException {
-        allInAllAzaguRajaStore.deleteAll();
-        allInAllAzaguRaja = new AllInAllAzaguRaja();
-        allInAllAzaguRaja.setA7DigitNumber(3);
-//        allInAllAzaguRaja.setVarchar("4");
-        allInAllAzaguRaja = allInAllAzaguRajaStore.insert()
-                .value(allInAllAzaguRaja).returning();
+
 
     }
 
     @Test
     void create() throws SQLException {
-        assertNotNull(allInAllAzaguRajaStore.find(allInAllAzaguRaja.getCode()),"Azagu Raja created");
-    }
+           }
 }
