99720a355689c54690ae6e8094e91cfcfdc0cf00
Haripriya <rm.haripriya@gmail.com>
2020-11-23 20:54:58 +0530

    testcase for AllInAllAzaguRaja

--- datastore/src/test/java/org/example/store/AllInAllAzaguRajaTest.java
+++ datastore/src/test/java/org/example/store/AllInAllAzaguRajaTest.java
@@ -1,11 +1,14 @@
 package org.example.store;
 
+import org.example.model.AllInAllAzaguRaja;
 import org.example.model.AllInAllAzaguRajaReference;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.example.util.DataSourceProvider.dataSource;
 
@@ -13,18 +16,23 @@ import static org.example.util.DataSourceProvider.dataSource;
 class AllInAllAzaguRajaTest {
 
     private final AllInAllAzaguRajaReferenceStore allAzaguRajaReferenceStore;
+    private final AllInAllAzaguRajaStore allInAllAzaguRajaStore;
+
 
     AllInAllAzaguRajaTest() {
         this.allAzaguRajaReferenceStore = new AllInAllAzaguRajaReferenceStore(dataSource());
+        this.allInAllAzaguRajaStore = new AllInAllAzaguRajaStore(dataSource());
     }
 
-    @BeforeAll
-    void init() throws SQLException {
 
-    }
 
     @Test
     void testAllInAllAzaguRaja() throws SQLException {
+//        this.allInAllAzaguRajaStore.deleteAll();
+//        this.allAzaguRajaReferenceStore.deleteAll();
+
+        List<AllInAllAzaguRaja> allinallazagurajaList = this.allInAllAzaguRajaStore.select();
+        Assertions.assertEquals(1, allinallazagurajaList.size(), "list of allinallazaghuraja");
 
     }
 }
