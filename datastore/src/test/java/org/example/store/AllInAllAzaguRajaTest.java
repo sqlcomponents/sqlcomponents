68c40e7b0ebf43d0c3185c64911a1ba3e54ac3f0
Haripriya <rm.haripriya@gmail.com>
2020-11-24 10:56:37 +0530

    testcase for AllInAllAzaguRaja with insert check

--- datastore/src/test/java/org/example/store/AllInAllAzaguRajaTest.java
+++ datastore/src/test/java/org/example/store/AllInAllAzaguRajaTest.java
@@ -2,10 +2,7 @@ package org.example.store;
 
 import org.example.model.AllInAllAzaguRaja;
 import org.example.model.AllInAllAzaguRajaReference;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -24,15 +21,37 @@ class AllInAllAzaguRajaTest {
         this.allInAllAzaguRajaStore = new AllInAllAzaguRajaStore(dataSource());
     }
 
-
+    @BeforeEach
+    void init() throws SQLException {
+        this.allInAllAzaguRajaStore.deleteAll();
+        this.allAzaguRajaReferenceStore.deleteAll();
+    }
 
     @Test
     void testAllInAllAzaguRaja() throws SQLException {
 //        this.allInAllAzaguRajaStore.deleteAll();
 //        this.allAzaguRajaReferenceStore.deleteAll();
+//        List<AllInAllAzaguRaja> allinallazagurajaList = this.allInAllAzaguRajaStore.select();
+//        Assertions.assertEquals(1, allinallazagurajaList.size(), "list of allinallazaghuraja");
 
-        List<AllInAllAzaguRaja> allinallazagurajaList = this.allInAllAzaguRajaStore.select();
-        Assertions.assertEquals(1, allinallazagurajaList.size(), "list of allinallazaghuraja");
+        //This is to check reference,--> insert by setting parameters.
+        AllInAllAzaguRajaReference azagurajaobject = new AllInAllAzaguRajaReference();
+        azagurajaobject.setCode("A110");
+        azagurajaobject.setName("Hari");
+        this.allAzaguRajaReferenceStore.insert(azagurajaobject);
 
+        //this is to check mymodel, --> insert by setting parameter.
+        AllInAllAzaguRaja allInAllAzaguRaja = new AllInAllAzaguRaja();
+        allInAllAzaguRaja.setReferenceCode("A110");
+        this.allInAllAzaguRajaStore.insert(allInAllAzaguRaja);
+
+//        //this is to check reference, --> find with a given code.
+//        AllInAllAzaguRajaReference insertedObject = this.allAzaguRajaReferenceStore.find("A110");
+//
+//        //this is to check reference, --> update by updating a parameter.
+//        insertedObject.setName("Priya");
+//        this.allAzaguRajaReferenceStore.update(insertedObject);
+//        Assertions.assertEquals("Priya", this.allAzaguRajaReferenceStore.find("A110").getName(), "reference updated successfully");
+//
     }
 }
