d404764ec848a6dc2750dd8395bb044fe5ef51aa
sathishk <sathishkumar.thiyagarajan@gmail.com>
2020-12-02 08:25:52 +0530

    Integer Tyoes

--- compiler/src/main/java/org/sqlcomponents/compiler/java/mapper/JavaMapper.java
+++ compiler/src/main/java/org/sqlcomponents/compiler/java/mapper/JavaMapper.java
@@ -3,8 +3,23 @@ package org.sqlcomponents.compiler.java.mapper;
 import org.sqlcomponents.core.mapper.Mapper;
 import org.sqlcomponents.core.model.relational.Column;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
 
-public class JavaMapper extends Mapper {
+
+public final class JavaMapper extends Mapper {
+
+    private final Map<Class<? extends Number>,Integer> integerTypes;
+
+    public JavaMapper() {
+        integerTypes = new IdentityHashMap<>();
+        // Holds Map of Unteger Types and Max digits they can hold
+        integerTypes.put(Byte.class,String.valueOf(Byte.MAX_VALUE).length()-1);
+        integerTypes.put(Short.class,String.valueOf(Short.MAX_VALUE).length()-1);
+        integerTypes.put(Integer.class,String.valueOf(Integer.MAX_VALUE).length()-1);
+        integerTypes.put(Long.class,String.valueOf(Long.MAX_VALUE).length()-1);
+
+    }
 
     @Override
     public String getDataType(Column column) {
@@ -20,13 +35,17 @@ public class JavaMapper extends Mapper {
             case VARCHAR:
                 return String.class;
             case BIT:
+            case BOOLEAN:
                 return Boolean.class;
         }
         throw new RuntimeException("Datatype not found for column "+ column);
     }
 
     private Class<? extends Number> chooseIntegerType(Column column) {
-        return column.getSize() > 2 ? Integer.class : Byte.class;
+        return integerTypes.entrySet().stream()
+                .filter( entry -> entry.getValue() <= column.getSize())
+                .max((entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()))
+                .get().getKey();
     }
 
 
