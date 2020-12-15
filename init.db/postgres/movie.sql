1fc50154748bd9eca3bed50fd694aed68ff4b1e8
haripriya <rm.haripriya@gmail.com>
2020-12-15 06:17:44 +0530

    Test fix

--- init.db/postgres/movie.sql
+++ init.db/postgres/movie.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS movie;
 
 CREATE TABLE movie (
     id smallserial PRIMARY KEY,
-    name VARCHAR(80),
+    title VARCHAR(80),
     directed_by VARCHAR(80),
     year_of_release NUMERIC(4),
     rating NUMERIC(2,1),
