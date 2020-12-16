f0d045b2be7fdb2f3dccc673bc8b0475144ae1b8
haripriya <rm.haripriya@gmail.com>
2020-12-16 11:17:03 +0530

    HikariDataSource

--- datastore/src/test/java/org/example/util/DataSourceProvider.java
+++ datastore/src/test/java/org/example/util/DataSourceProvider.java
@@ -1,12 +1,11 @@
 package org.example.util;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
-import org.postgresql.ds.PGSimpleDataSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public final class DataSourceProvider {
@@ -21,28 +20,11 @@ public final class DataSourceProvider {
         String databaseType = System.getenv("DATABASE_TYPE") == null
                 ? "postgres" : System.getenv("DATABASE_TYPE");
 
-        return getDataSource(props.getProperty(databaseType+".datasource.url")
-        ,props.getProperty(databaseType+".datasource.username"),
-                props.getProperty(databaseType+".datasource.password"));
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(props.getProperty(databaseType+".datasource.url"));
+        config.setUsername(props.getProperty(databaseType+".datasource.username"));
+        config.setPassword(props.getProperty(databaseType+".datasource.password"));
+        return new HikariDataSource(config);
     }
 
-    public static DataSource getDataSource(final String url
-            ,final String userName
-            ,final String password) {
-        if(url.startsWith("jdbc:postgresql:")) {
-            PGSimpleDataSource ds = new PGSimpleDataSource();
-            ds.setURL(url);
-            ds.setUser(userName);
-            ds.setPassword(password);
-            return ds;
-        }
-        if(url.startsWith("jdbc:mysql:")) {
-            MysqlDataSource mySQLDataSource = new MysqlDataSource();
-            mySQLDataSource.setUrl(url);
-            mySQLDataSource.setUser(userName);
-            mySQLDataSource.setPassword(password);
-            return mySQLDataSource;
-        }
-        return null;
-    }
 }
