d8740da1fdf1ddf4e6851f6b6e6490f76480c9c4
haripriya <rm.haripriya@gmail.com>
2020-12-17 17:39:58 +0530

    Mysql MariaDB is working now

--- init.db/mariadb/movie.sql
+++ init.db/mariadb/movie.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS movie;
 CREATE TABLE movie (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(80),
-    directed_by VARCHAR(80)
+    directed_by VARCHAR(80),
+    PRIMARY KEY ( id )
 );
