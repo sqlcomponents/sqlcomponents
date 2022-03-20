1400618796184b188df4bac652eeab4d9df6e5ab
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-03-20 13:47:17 +0530

    Code Clean Up

--- datastore/src/test/java/org/example/util/JsonUtil.java
+++ datastore/src/test/java/org/example/util/JsonUtil.java
@@ -1,28 +1,21 @@
 package org.example.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
-import com.fasterxml.jackson.datatype.jsr353.JSR353Module;
-import org.example.model.AzaguRaja;
-import org.json.JSONObject;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public final class JsonUtil {
 
-    private static ObjectMapper mapper ;
-    public static <T> List<T> getTestObjects(Class<T> tClass)  {
+    private static ObjectMapper mapper;
+
+    public static <T> List<T> getTestObjects(Class<T> tClass) {
         String databaseType = System.getenv("DATABASE_TYPE") == null
                 ? "postgres" : System.getenv("DATABASE_TYPE");
 
@@ -30,11 +23,11 @@ public final class JsonUtil {
                 = new ArrayList<>();
 
 
-        File jsonFile  = new File("../datastore/src/test/resources/data/"+databaseType+"/"
-                +tClass.getName().substring(tClass.getName().lastIndexOf('.')+1)+".json");
-        if(!jsonFile.exists()) {
-            jsonFile  = new File("../datastore/src/test/resources/data/"
-                    +tClass.getName().substring(tClass.getName().lastIndexOf('.')+1)+".json");
+        File jsonFile = new File("../datastore/src/test/resources/data/" + databaseType + "/"
+                + tClass.getName().substring(tClass.getName().lastIndexOf('.') + 1) + ".json");
+        if (!jsonFile.exists()) {
+            jsonFile = new File("../datastore/src/test/resources/data/"
+                    + tClass.getName().substring(tClass.getName().lastIndexOf('.') + 1) + ".json");
         }
 
         try (JsonParser jsonParser = getObjectMapper().getFactory()
@@ -43,11 +36,11 @@ public final class JsonUtil {
                 throw new IllegalArgumentException(
                         "illicalstate of array", null);
             }
-                while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
-                    T entity = getObjectMapper()
-                            .readValue(jsonParser, tClass);
-                    list.add(entity);
-                }
+            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                T entity = getObjectMapper()
+                        .readValue(jsonParser, tClass);
+                list.add(entity);
+            }
 
         } catch (IOException e) {
             /* Unreachable */
@@ -57,8 +50,7 @@ public final class JsonUtil {
     }
 
 
-
-    public static String getJSONString(Object obj)  {
+    public static String getJSONString(Object obj) {
         try {
             return getObjectMapper().writeValueAsString(obj);
         } catch (JsonProcessingException e) {
@@ -68,7 +60,7 @@ public final class JsonUtil {
     }
 
     private static ObjectMapper getObjectMapper() {
-        if(mapper == null) {
+        if (mapper == null) {
             mapper = new ObjectMapper();
             mapper.findAndRegisterModules();
             mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
