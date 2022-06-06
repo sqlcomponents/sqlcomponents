e95857b3e0c76e3ba0fb5b1adec8e01eda05353d
sathishk <sathishkumar.thiyagarajan@gmail.com>
2022-06-06 23:04:09 +0530

    Pluggable Test Database

--- compiler/src/test/java/org/sqlcomponents/compiler/java/JavaCompilerTest.java
+++ compiler/src/test/java/org/sqlcomponents/compiler/java/JavaCompilerTest.java
@@ -3,7 +3,9 @@ package org.sqlcomponents.compiler.java;
 import org.junit.jupiter.api.Test;
 import org.sqlcomponents.core.exception.ScubeException;
 import org.sqlcomponents.core.model.Application;
+import org.sqlcomponents.core.utils.CoreConsts;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -12,34 +14,42 @@ class JavaCompilerTest {
     @Test
     void writeCode() throws ScubeException, IOException {
 
-		final String srcFolder = System.getenv("SOURCE_FOLDER") == null
-				? "../datastore/src/main/java" : System.getenv("SOURCE_FOLDER");
+		Application application ;
 
-		final Application application = new Application();
+		if(System.getenv("SQLCOMPONENTS_CONFIG") == null) {
+			application = new Application();
+			Properties props = new Properties();
+			props.load(new FileReader("../database.properties"));
 
-		Properties props = new Properties();
-		props.load(new FileReader("../database.properties"));
+			String databaseType = System.getenv("DATABASE_TYPE") == null
+					? "postgres" : System.getenv("DATABASE_TYPE");
 
-		String databaseType = System.getenv("DATABASE_TYPE") == null
-				? "postgres" : System.getenv("DATABASE_TYPE");
+			application.setName("Movie");
+			application.setUrl(props.getProperty(databaseType + ".datasource.url"));
+			application.setUserName(props.getProperty(databaseType + ".datasource.username"));
+			application.setPassword(props.getProperty(databaseType + ".datasource.password"));
+			application.setSchemaName(props.getProperty(databaseType + ".datasource.schema"));
+			// daoProject.setTablePatterns(Arrays.asList("movie"));
 
-		application.setName("Movie");
-		application.setUrl(props.getProperty(databaseType + ".datasource.url"));
-		application.setUserName(props.getProperty(databaseType + ".datasource.username"));
-		application.setPassword(props.getProperty(databaseType + ".datasource.password"));
-		application.setSchemaName(props.getProperty(databaseType + ".datasource.schema"));
-		// daoProject.setTablePatterns(Arrays.asList("movie"));
+			application.setOnline(true);
+			application.setBeanIdentifier("model");
+			application.setDaoIdentifier("store");
+			application.setDaoSuffix("");
+			application.setRootPackage("org.example");
+			application.setCleanSource(true);
 
-		application.setOnline(true);
-		application.setBeanIdentifier("model");
-		application.setDaoIdentifier("store");
-		application.setDaoSuffix("");
-		application.setRootPackage("org.example");
-		application.setCleanSource(true);
+			application.setMethodSpecification(Application.METHOD_SPECIFICATION);
 
-		application.setMethodSpecification(Application.METHOD_SPECIFICATION);
+			application.setSrcFolder("../datastore/src/main/java");
 
-		application.setSrcFolder(srcFolder);
+
+		} else {
+			application = CoreConsts.buildApplication(new File(System.getenv("SQLCOMPONENTS_CONFIG")));
+			if(System.getenv("SOURCE_FOLDER") == null) {
+				throw new IllegalArgumentException("SOURCE_FOLDER is not set");
+			}
+			application.setSrcFolder(System.getenv("SOURCE_FOLDER"));
+		}
 		application.compile(new JavaFTLCompiler());
 		System.out.println("Code is compiled into " + application.getSrcFolder());
     }
