4bb34843fc1e983658a9de24b50065f645d585be
haripriya <rm.haripriya@gmail.com>
2022-08-04 18:59:38 +0530

    Formatter Issue 2

--- compiler/src/test/java/org/sqlcomponents/compiler/java/JavaCompilerTest.java
+++ compiler/src/test/java/org/sqlcomponents/compiler/java/JavaCompilerTest.java
@@ -11,17 +11,22 @@ class JavaCompilerTest {
     @Test
     void writeCode() throws Exception {
         Application application = CompilerTestUtil.getApplication();
-        Map<String, String> insertMap = new HashMap<>();
-        insertMap.put("created_at", "CURRENT_TIMESTAMP");
-        insertMap.put("modified_by", "");
-        insertMap.put("modified_at", "");
-        application.setInsertMap(insertMap);
 
-        Map<String, String> updateMap = new HashMap<>();
-        updateMap.put("modified_at", "CURRENT_TIMESTAMP");
-        updateMap.put("created_by", "");
-        updateMap.put("created_at", "");
-        application.setUpdateMap(updateMap);
+        if (application.getInsertMap() == null) {
+            Map<String, String> insertMap = new HashMap<>();
+            insertMap.put("created_at", "CURRENT_TIMESTAMP");
+            insertMap.put("modified_by", "");
+            insertMap.put("modified_at", "");
+            application.setInsertMap(insertMap);
+        }
+
+        if (application.getUpdateMap() == null) {
+            Map<String, String> updateMap = new HashMap<>();
+            updateMap.put("modified_at", "CURRENT_TIMESTAMP");
+            updateMap.put("created_by", "");
+            updateMap.put("created_at", "");
+            application.setUpdateMap(updateMap);
+        }
 
         application.compile(new JavaCompiler());
         System.out.println("Code is compiled into " + application.getSrcFolder());
