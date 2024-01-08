b781c4bc6db2afac259767869d34b4c64f656908
Sathish Kumar Thiyagarajan <sathishk@users.noreply.github.com>
2024-01-08 08:45:17 +0530

    Create JavaMapperTest.java

---
+++ compiler/src/test/java/org/sqlcomponents/compiler/java/mapper/JavaMapperTest.java
@@ -0,0 +1,39 @@
+package org.sqlcomponents.compiler.java.mapper;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.sqlcomponents.compiler.java.util.CompilerTestUtil;
+import org.sqlcomponents.core.crawler.Crawler;
+import org.sqlcomponents.core.model.Application;
+import org.sqlcomponents.core.model.relational.Database;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaMapperTest {
+
+    private final Database database;
+    private final JavaMapper javaMapper;
+
+    JavaMapperTest() throws IOException, SQLException {
+        Application application = CompilerTestUtil.getApplication();
+        this.database = new Crawler(application).getDatabase();
+        this.javaMapper = new JavaMapper(application);
+    }
+
+    @Test
+    void getDataType() throws Exception {
+        assertEquals(UUID.class, Class.forName(getDataType("a_uuid")), "Type Mismatch");
+    }
+
+    @NotNull
+    private String getDataType(final String columnName) {
+        return javaMapper.getDataType(database.getTables().stream()
+                .filter(table -> table.getTableName().equals("raja")).findFirst()
+                .get().getColumns().stream()
+                .filter(column -> column.getColumnName().equals(columnName)).findFirst().get());
+    }
+}
