55a6602e91a0770102e0f41226a8f0025201bafd
sathishk <sathishkumar.thiyagarajan@gmail.com>
2024-08-15 09:25:08 +0530

    Fixed Build Issues 2

--- init.db/postgres/raja.sql
+++ init.db/postgres/raja.sql
@@ -6,20 +6,25 @@ CREATE TABLE connection (
     name VARCHAR(80),
     UNIQUE(name)
 );
-
+-- Table that consistas one per postgres datatype example
+-- Ref: https://www.postgresql.org/docs/current/datatype.html
 CREATE TABLE raja(
    id SERIAL PRIMARY KEY,
+   -- Numeric Types
+   -- Ref: https://www.postgresql.org/docs/current/datatype-numeric.html
+    a_smallint smallint CHECK (a_smallint>=0),
+    a_integer	 integer CHECK (a_integer >= 0),
+    a_bigint	 bigint	CHECK (a_bigint >= 0),
+    a_decimal	 decimal,
+    a_numeric	 numeric,
+
    a_boolean boolean,
    reference_code uuid,
    a_char CHAR(1),
    a_bpchar bpchar,
    a_text text,
    a_encrypted_text text,
-	a_smallint smallint CHECK (a_smallint>=0),
-	a_integer	 integer CHECK (a_integer >= 0),	
-	a_bigint	 bigint	CHECK (a_bigint >= 0),
-	a_decimal	 decimal,	
-	a_numeric	 numeric,
+
 	a_money money,
 	a_real real,
 	a_double  DOUBLE PRECISION, 
@@ -36,13 +41,16 @@ CREATE TABLE raja(
     a_interval interval,
      a_macaddr8 macaddr8,
     a_lseg lseg,
---    a_inet inet,
-    a_point point,
     a_inet inet,
---    a_point point,
- a_circle circle,
     a_cidr cidr,
     a_macaddr macaddr,
+    -- Ceometric Types
+    -- Ref: https://www.postgresql.org/docs/current/datatype-geometric.html
+    a_point point,
+    a_inet inet,
+    --TODO a_point point,
+ a_circle circle,
+
     a_polygon polygon,
     a_path path,
     a_line line,
