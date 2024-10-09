34345982051e4c1992cdec342e04d1ddddcad8dd
sathishk <sathishkumar.thiyagarajan@gmail.com>
2024-10-09 16:07:18 +0530

    SSSdd

--- datastore/src/test/java/org/example/store/CountTest.java
+++ datastore/src/test/java/org/example/store/CountTest.java
@@ -1,4 +1,50 @@
 package org.example.store;
 
-public class CountTest {
+import org.example.DataManager;
+import org.example.model.Movie;
+import org.example.util.DataSourceProvider;
+import org.example.util.EncryptionUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.example.store.MovieStore.*;
+
+class CountTest {
+
+    private final MovieStore movieStore;
+
+    CountTest() {
+
+        DataManager dataManager =
+                DataManager.getManager(DataSourceProvider.dataSource(),
+                        EncryptionUtil::enAnDecrypt,
+                        EncryptionUtil::enAnDecrypt);
+
+        this.movieStore = dataManager.getMovieStore();
+    }
+
+
+    @BeforeEach
+    void init() throws SQLException {
+        this.movieStore.delete().execute();
+    }
+
+    @Test
+    void testSql() throws SQLException {
+
+        // Single Primary Key
+        this.movieStore.insert().values(new Movie(null,"Vettayan","Gyanavel")).execute();
+        Assertions.assertEquals(1, this.movieStore.select().count());
+
+        // Multiple Primary Keys
+
+        // No Primary Key (Code Table)
+
+
+    }
 }
